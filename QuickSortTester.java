4dd1ccb22f542e78d09401b3c618d8517c4e26a5
JonathanQuang <jquang@stuy.edu>
2017-03-09 19:36:38 -0500

    updated some of the commented information

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -8,8 +8,9 @@
 
   Quick Sort ALGORITHM:
    1. If bounds overlap stop.
-   2. pick center as pivot point to create a partition about.
-   3. call quick sort on two halves from the partition
+   2. pick center of the bounds as the target index in the pivot function
+   3. Call quick sort on the two parts produced by the partition index returned
+      from the pivot function.
 
   BIG-OH CLASSIFICATION OF ALGORITHM:
   O(nlog(n))
@@ -38,7 +39,13 @@
 
   Sidenote
 
-  After running multiple runtime tests, we noticed that the runtime of the first few executions of MergeSort were significantly larger than runtimes of successive executions. We found that this is because java was still initializing. In order to get more accurate data, we added another set of tests of size 10000000 that does not get displayed on the terminal, so that the successive runtimes are more accurate. We used size 10000000 because this properly fixed the runtimes. When we tried shorter lists it didn't really have much of an effect. 
+  After running multiple runtime tests, we noticed that the runtime of the first few
+ executions of QuickSort were significantly larger than runtimes of successive 
+ executions. We found that this is because java was still initializing. To resolve
+ this issue, we run a set of tests of size 10000000 that does not get displayed
+ in the terminal.
+
+
   
   ANALYSIS:
   In order to get some data, we calculated the average runtime of lists between size 50000 and 1000000 with increments of 50000. We then took the average runtimes and graphed them. We also graphed n, log(n), nlog(n), and n^2 runtime for comparison. The graph looked most similar to nlog(n). To confirm the runtime, we graphed lines of xnlog(n) replacing x with some arbitray number to see if it was close to matching. We also graphed lines of xlog(n) and xn to compare. From our testing we found that our data is most closely represented by the line 13nlog(n). We then concluded that the runtime of merge sort is nlog(n).
